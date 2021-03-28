6016af2e2f702d591c3ec16208e1c415efeeee35
Sai rohith gorla <sairohithgorla01@gmail.com>
2021-03-28 16:32:18 -0500

    created and added the router.js

---
+++ routes/router.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const router = express.Router()
+router.use(bodyParser.urlencoded({ extended: true }));
+require('dotenv').config();
+const locationController = require('../controllers/locationController');
+// const display = require('../views/display/display.js');
+// const edit = require('../views/edit/edit.js')
+// const create = require('../views/create/create.js')
+const services = require('../services')
+
+
+router.get('/', (req, res, next) => {
+    res.render('../views/index', { title: 'index' })
+})
+
+
+
+router.get('/locations/', services.displays);
+router.get('/locations/editview/:id', services.updateLocation);
+router.get('/locations/create', services.addlocation);
+
+// router.get('/display', services.displays);
+// router.get('/editview', services.updateLocation);
+// router.get('/create', services.addlocation);
+
+
+router.get('/location/', locationController.findAll);
+
+router.post('/locations/', locationController.create);
+
+router.post('/locations/edit/:id', locationController.update)
+
+router.post('/locations/delete/:id', locationController.delete)
+
+
+
+
+
+
+module.exports = router;
